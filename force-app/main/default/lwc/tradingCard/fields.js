783052af9dc8d4f4fc48f19e8ea536be08ade4f7
Julian Duque <julianduquej@gmail.com>
2021-01-12 19:21:06 -0500

    refactor salesforce app using custom lwc

---
+++ force-app/main/default/lwc/tradingCard/fields.js
@@ -0,0 +1,26 @@
+// Fields
+import NAME_FIELD from "@salesforce/schema/Card__c.Name";
+import BIO_FIELD from "@salesforce/schema/Card__c.Bio__c";
+import STRENGTHS_FIELD from "@salesforce/schema/Card__c.Feats_of_Strength__c";
+import PICTURE_PATH_FIELD from "@salesforce/schema/Card__c.Picture_Path__c";
+import EMAIL_FIELD from "@salesforce/schema/Card__c.Email__c";
+import MAIN_WEBSITE_FIELD from "@salesforce/schema/Card__c.Main_Website__c";
+import GITHUB_FIELD from "@salesforce/schema/Card__c.GitHub_Username__c";
+import TWITTER_FIELD from "@salesforce/schema/Card__c.Twitter_Username__c";
+import LINKEDIN_FIELD from "@salesforce/schema/Card__c.LinkedIn_Username__c";
+import INSTAGRAM_FIELD from "@salesforce/schema/Card__c.Instagram_Username__c";
+import CODEPEN_FIELD from "@salesforce/schema/Card__c.CodePen_Username__c";
+
+export {
+  NAME_FIELD,
+  EMAIL_FIELD,
+  BIO_FIELD,
+  STRENGTHS_FIELD,
+  PICTURE_PATH_FIELD,
+  MAIN_WEBSITE_FIELD,
+  GITHUB_FIELD,
+  TWITTER_FIELD,
+  LINKEDIN_FIELD,
+  INSTAGRAM_FIELD,
+  CODEPEN_FIELD
+};
