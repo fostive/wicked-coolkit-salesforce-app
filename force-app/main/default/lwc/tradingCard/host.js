34e72a2e1db99cc31cf89f59bc4bd59361175d9f
Julián Duque <julianduquej@gmail.com>
2021-01-21 16:46:56 -0500

    Implement stickers form (#25)
    * Implement stickers form
    * make it 12 stickers
    * simplify stickers by card query

--- force-app/main/default/lwc/tradingCard/host.js
+++ force-app/main/default/lwc/tradingCard/host.js
@@ -1,14 +1,7 @@
-const isLocal = () => window.location.host === "localhost:3001";
-
-export const isValid = (host) => isLocal() || !!host;
-
 export const api = (host) => {
-  const protocol = isLocal() ? "http" : "https";
-  return `${protocol}://${host || window.location.host}/api`;
+  return `$https://${host}/api`;
 };
 
 export const sticker = (s) => {
-  return isLocal()
-    ? `/stickers/svg/${s}.svg`
-    : `https://unpkg.com/wicked-coolkit/dist/stickers/svg/${s}.svg`;
+  return `https://unpkg.com/wicked-coolkit/dist/stickers/svg/${s}.svg`;
 };
