75623a873933697b7951788e70aec1e013abd080
Chris Castle <chris.castle@salesforce.com>
2021-01-23 20:37:40 -0500

    When adding new website 1) auto-populate webring field 2) refresh website list

--- force-app/main/default/lwc/createWebsite/createWebsite.js
+++ force-app/main/default/lwc/createWebsite/createWebsite.js
@@ -1,25 +1,45 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { publish, MessageContext } from "lightning/messageService";
 
 import WEBSITE_OBJECT from "@salesforce/schema/Website__c";
 import NAME_FIELD from "@salesforce/schema/Website__c.Name";
 import URL_FIELD from "@salesforce/schema/Website__c.URL__c";
 import WEBRING_FIELD from "@salesforce/schema/Website__c.Webring__c";
+import WEBSITE_ADDED from "@salesforce/messageChannel/Website_Added__c";
 
 /**
  * Creates Website records.
  */
 export default class CreateWebsite extends LightningElement {
+  @api webringId;
   websiteObject = WEBSITE_OBJECT;
   fields = [NAME_FIELD, URL_FIELD, WEBRING_FIELD];
-  mode = "edit";
+  subscription;
+
+  @wire(MessageContext)
+  messageContext;
 
   handleSuccess() {
+    // Show notification
     const toastEvt = new ShowToastEvent({
       title: "Website created",
       message: "Your website has been created and added to webring",
       variant: "success"
     });
     this.dispatchEvent(toastEvt);
+
+    // Publish message so list of websites can update
+    publish(this.messageContext, WEBSITE_ADDED);
+
+    // Reset fields
+    const inputFields = this.template.querySelectorAll("lightning-input-field");
+    if (inputFields) {
+      inputFields.forEach((field) => {
+        if (!field.disabled) {
+          field.reset();
+        }
+      });
+    }
   }
 }
--- force-app/main/default/lwc/webringList/webringList.js
+++ force-app/main/default/lwc/webringList/webringList.js
@@ -3,10 +3,16 @@ import getWebsitesByWebring from "@salesforce/apex/WebsiteController.getWebsites
 import { updateRecord } from "lightning/uiRecordApi";
 import { refreshApex } from "@salesforce/apex";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import {
+  subscribe,
+  unsubscribe,
+  MessageContext
+} from "lightning/messageService";
 
 import WEBSITE_ID_FIELD from "@salesforce/schema/Website__c.Id";
 import WEBSITE_URL_FIELD from "@salesforce/schema/Website__c.URL__c";
 import WEBSITE_NAME_FIELD from "@salesforce/schema/Website__c.Name";
+import WEBSITE_ADDED from "@salesforce/messageChannel/Website_Added__c";
 
 const columns = [
   { label: "URL", fieldName: "URL__c", editable: true },
@@ -17,9 +23,14 @@ export default class WebringList extends LightningElement {
   @api webringId;
   columns = columns;
   websiteList;
+  subscription;
+  websitesByWebringResponse;
 
   @wire(getWebsitesByWebring, { webringId: "$webringId" })
-  websites({ data, error }) {
+  websites(response) {
+    this.websitesByWebringResponse = response;
+
+    const { data, error } = response;
     if (error) {
       console.log("Error getting list of Websites in Webring:", error);
     } else if (data) {
@@ -29,6 +40,30 @@ export default class WebringList extends LightningElement {
 
   get emptyWebsiteList() {
     return !this.websiteList || this.websiteList.length === 0;
+  }
+
+  @wire(MessageContext)
+  messageContext;
+
+  subscribeToMessageChannel() {
+    if (!this.subscription) {
+      this.subscription = subscribe(this.messageContext, WEBSITE_ADDED, () => {
+        refreshApex(this.websitesByWebringResponse);
+      });
+    }
+  }
+
+  unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+  }
+
+  connectedCallback() {
+    this.subscribeToMessageChannel();
+  }
+
+  disconnectedCallback() {
+    this.unsubscribeToMessageChannel();
   }
 
   handleUpdate(event) {
