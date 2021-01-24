d9ffda875a30b160cfdf17b190cb21eb465f4d14
Chris Castle <chris.castle@salesforce.com>
2021-01-23 22:42:38 -0500

    Handle multiple webrings

--- force-app/main/default/lwc/webringHome/webringHome.js
+++ force-app/main/default/lwc/webringHome/webringHome.js
@@ -1,24 +1,38 @@
 import { LightningElement, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import getWebring from "@salesforce/apex/WebringController.getWebring";
+import getWebrings from "@salesforce/apex/WebringController.getWebrings";
 import WEBRING_OBJECT from "@salesforce/schema/Webring__c";
 import WEBRING_NAME_FIELD from "@salesforce/schema/Webring__c.Name";
 import WEBRING_DESCRIPTION_FIELD from "@salesforce/schema/Webring__c.Description__c";
 
 export default class WebringHome extends LightningElement {
   webringId;
+  webringList;
   webringObject = WEBRING_OBJECT;
   webringFields = [WEBRING_NAME_FIELD, WEBRING_DESCRIPTION_FIELD];
   mode = "edit";
+  webringsResponse;
 
-  @wire(getWebring, {})
-  webring({ error, data }) {
+  @wire(getWebrings)
+  webrings(response) {
+    this.webringsResponse = response;
+
+    const { data, error } = response;
     if (error) {
-      console.log("Error querying Webring(s):", error);
+      console.log("Error getting list of Webring(s):", error);
     } else if (data && data[0]) {
       this.webringId = data[0].Id;
+
+      this.webringList = data.map((webring) => ({
+        value: webring.Id,
+        label: webring.Name
+      }));
     }
+  }
+
+  handleWebringChange(event) {
+    this.webringId = event.detail.value;
   }
 
   handleCreateSuccess() {
@@ -38,9 +52,5 @@ export default class WebringHome extends LightningElement {
 
   get oneWebring() {
     return typeof this.webringId === "string";
-  }
-
-  get multipleWebrings() {
-    return Array.isArray(this.webringId);
   }
 }
