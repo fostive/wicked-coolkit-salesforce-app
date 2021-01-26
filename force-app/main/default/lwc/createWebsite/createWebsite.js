8bf854d30dab628fbcbcfe5500d1e6af318e09d2
Chris Castle <castle@heroku.com>
2021-01-26 18:01:30 -0500

    Webring UI (#26)
    * WIP: Webring setup UI
    * Handle zero, one, or multiple webrings
    * When adding new website 1) auto-populate webring field 2) refresh website list
    * Allow deleting rows and fix editing rows
    * Handle multiple webrings
    * Webring UI improvements
    * Refresh Apex after creating first webring
    * Code cleanup
    * Use loading spinner
    * Only allow creation and use of 1 webring
    * Fix datatable inline edit bug
    * Toast text tweak
    * Remove stickers, websites, and webrings default nav tabs
    * Remove old code
    * Show Webring tab by defaul
    * some style tweaks for webring UI
    * small tweak to trading card
    * Move webring instructions to lwc code
    * Improve UX and error handling
    * styling for intro section
    * Update instructions until dynamic embed code can be displayed
    
    Co-authored-by: Lynn Fisher <lynnfishe@gmail.com>

---
+++ force-app/main/default/lwc/createWebsite/createWebsite.js
@@ -0,0 +1,60 @@
+import { LightningElement, wire, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { publish, MessageContext } from "lightning/messageService";
+
+import WEBSITE_OBJECT from "@salesforce/schema/Website__c";
+import NAME_FIELD from "@salesforce/schema/Website__c.Name";
+import URL_FIELD from "@salesforce/schema/Website__c.URL__c";
+import WEBRING_FIELD from "@salesforce/schema/Website__c.Webring__c";
+import WEBSITE_ADDED from "@salesforce/messageChannel/Website_Added__c";
+
+/**
+ * Creates Website records.
+ */
+export default class CreateWebsite extends LightningElement {
+  websiteObject = WEBSITE_OBJECT;
+  nameField = NAME_FIELD;
+  urlField = URL_FIELD;
+  webringField = WEBRING_FIELD;
+
+  fields = [NAME_FIELD, URL_FIELD, WEBRING_FIELD];
+
+  @api webringId;
+  subscription;
+  disabled = false;
+
+  @wire(MessageContext)
+  messageContext;
+
+  onSubmit() {
+    this.disabled = true;
+  }
+
+  onError() {
+    this.disabled = false;
+  }
+
+  handleSuccess() {
+    this.disabled = false;
+
+    // Show notification
+    const toastEvt = new ShowToastEvent({
+      title: "Website added",
+      variant: "success"
+    });
+    this.dispatchEvent(toastEvt);
+
+    // Publish message so list of websites can update
+    publish(this.messageContext, WEBSITE_ADDED);
+
+    // Reset fields
+    const inputFields = this.template.querySelectorAll("lightning-input-field");
+    if (inputFields) {
+      inputFields.forEach((field) => {
+        if (field.fieldName !== WEBRING_FIELD.fieldApiName) {
+          field.reset();
+        }
+      });
+    }
+  }
+}
---
+++ force-app/main/default/lwc/webringHome/webringHome.js
@@ -0,0 +1,56 @@
+import { LightningElement, wire } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { refreshApex } from "@salesforce/apex";
+
+import getWebrings from "@salesforce/apex/WebringController.getWebrings";
+import WEBRING_OBJECT from "@salesforce/schema/Webring__c";
+import WEBRING_NAME_FIELD from "@salesforce/schema/Webring__c.Name";
+import WEBRING_DESCRIPTION_FIELD from "@salesforce/schema/Webring__c.Description__c";
+
+const embedCode = `<script type="module" async src="https://unpkg.com/wicked-coolkit/dist/webring.js"></script>
+<wck-webring host="lukeswickedcoolkit.herokuapp.com"></wck-webring>`;
+
+export default class WebringHome extends LightningElement {
+  loading = true;
+  webringId;
+  webringObject = WEBRING_OBJECT;
+  webringFields = [WEBRING_NAME_FIELD, WEBRING_DESCRIPTION_FIELD];
+  mode = "edit";
+  webringsResponse;
+  embedCode = embedCode;
+
+  @wire(getWebrings)
+  webrings(response) {
+    this.webringsResponse = response;
+
+    const { data, error } = response;
+    if (error) {
+      console.log("Error getting list of Webring(s):", error);
+    } else if (data && data[0]) {
+      this.webringId = data[0].Id;
+    }
+  }
+
+  handleCreateSuccess() {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "Webring created",
+        variant: "success"
+      })
+    );
+
+    refreshApex(this.webringsResponse);
+  }
+
+  connectedCallback() {
+    this.loading = false;
+  }
+
+  get zeroWebrings() {
+    return this.webringId === undefined;
+  }
+
+  get oneWebring() {
+    return typeof this.webringId === "string";
+  }
+}
